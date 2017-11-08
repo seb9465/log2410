fd1e97e7f0654b35d71f0eec869a6f7570682e45
seb9465 <cado.94@me.com>
2017-11-08 16:43:46 -0500

    SC : Ajout de commentaires.

--- tp4/TP4-A17/CompositeTransform.cpp
+++ tp4/TP4-A17/CompositeTransform.cpp
@@ -9,7 +9,6 @@
 
 std::vector<std::shared_ptr<AbsTransform>> AbsTransform::m_empty_transforms;
 
-
 // Cloner la transformation composite et ses commandes enfant
 CompositeTransform * CompositeTransform::clone(void) const{
 	CompositeTransform* newClone = new CompositeTransform;
