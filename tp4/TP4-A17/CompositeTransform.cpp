008329fa0d84065bed6ac39c3c6849edee9534d7
seb9465 <cado.94@me.com>
2017-11-08 16:39:52 -0500

    SC : Ajout de commentaires.

--- tp4/TP4-A17/CompositeTransform.cpp
+++ tp4/TP4-A17/CompositeTransform.cpp
@@ -11,18 +11,17 @@ std::vector<std::shared_ptr<AbsTransform>> AbsTransform::m_empty_transforms;
 
 
 // Cloner la transformation composite et ses commandes enfant
-CompositeTransform * CompositeTransform::clone(void) const
-{
+CompositeTransform * CompositeTransform::clone(void) const{
 	CompositeTransform* newClone = new CompositeTransform;
 
+	// On fait une copie des transformations vers l'objet cloné.
 	newClone->m_transforms = this->m_transforms;
 
 	return newClone;
 }
 
 // Executer les commandes enfant
-void CompositeTransform::transform(const Chunk_iterator& c, AbsAudioFile& outFile) const
-{
+void CompositeTransform::transform(const Chunk_iterator& c, AbsAudioFile& outFile) const{
 	// On applique la transformation pour chaque transformation se trouvant dans le vecteur 'm_transform'.
 	for (auto i : m_transforms)
 		i->transform(c, outFile);
