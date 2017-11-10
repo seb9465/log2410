205ff8bffeee18e998fd32ffa644018657a239f0
seb9465 <cado.94@me.com>
2017-11-09 20:27:33 -0500

    SC : tp4.

--- tp4/TP4-A17/RepeatTransform.cpp
+++ tp4/TP4-A17/RepeatTransform.cpp
@@ -2,11 +2,11 @@
 
 void RepeatTransform::transform(const Chunk_iterator& c, AbsAudioFile& outFile) const {
 	// On ajoute m_nRepetitions fois le Chunk au fichier de sortie.
-	for (int i = 0; i < m_nRepetitions; i++)
+
+	for (size_t i = 0; i < getNRepetitions(); i++)
 		outFile.addChunk(c);
 }
 
-size_t RepeatTransform::getNRepetitions(void) {
+size_t RepeatTransform::getNRepetitions(void) const {
 	return m_nRepetitions;
 }
-
