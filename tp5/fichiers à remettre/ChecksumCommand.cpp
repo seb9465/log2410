c83f40caad5ba5d8357c16504433e65eb7a8e89b
seb9465 <cado.94@me.com>
2017-12-03 20:32:38 -0500

    SC : Verion finale du tp5 remise sur Moodle.

--- tp5/fichiers à remettre/ChecksumCommand.cpp
+++
@@ -1,7 +0,0 @@
-#include "ChecksumCommand.h"
-#include "AbsAudioFile.h"
-
-void ChecksumCommand::execute(AbsAudioFile & f)
-{
-	f.accept(m_visitor);
-}
--- tp5/fichiers à remettre/StringFindReplaceCommand.cpp
+++
@@ -1,7 +0,0 @@
-#include "StringFindReplaceCommand.h"
-#include "AbsAudioFile.h"
-
-void StringFindReplaceCommand::execute(AbsAudioFile & f)
-{
-	f.accept(m_visitor);
-}
